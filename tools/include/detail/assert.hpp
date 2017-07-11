16277d20f76b314aaf8bb0705b48572badaf3082
daramos <dan.ramos@nokia.com>
2017-07-11 19:31:32 +0800

    test tool changes

--- tools/include/detail/assert.hpp
+++ tools/include/detail/assert.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdexcept>
 #include <detail/test_exception.hpp>
+#include <vector>
 
 namespace test
 {
@@ -20,6 +21,24 @@ void assert_equal_impl(const A& a, const B& b,
                   << " ]\n    values: [ " << a << " != " << b  << " ] " << std::endl;
         throw test_exception();
     }
+}
+
+template <typename T>
+void assert_equal_vectors_impl(const std::vector<T>& a, const std::vector<T>& b,
+        const char* a_label,
+        const char* b_label,
+        int line_number,
+        const char* filename)
+{
+    if (a.size() != b.size()) {
+        std::cout << "\x1B[1m\x1B[31m[ERROR ] Assertion failed at line " <<filename << ":" << line_number
+                  << ": \x1B[0m\n"
+                  << "    expression: [ " << a_label << " != " << b_label << " ]\n"
+                  << "    sizes: [ " << a.size() << " != " << b.size()  << " ] " << std::endl;
+        throw test_exception();
+    }
+
+
 }
 
 } 
--- tools/include/dojo-test-tools.hpp
+++ tools/include/dojo-test-tools.hpp
@@ -12,6 +12,7 @@
 #include <detail/TestRunner.hpp>
 
 #define ASSERT_EQUAL(a, b) test::detail::assert_equal_impl(a, b, #a, #b,  __LINE__, __FILE__);
+#define ASSERT_EQUAL_VECTORS(a, b) test::detail::assert_equal_vectors_impl(a, b , #a, #b,  __LINE__, __FILE__);
 #define NOT_IMPLEMENTED test::assert_impl(__LINE__, __FILE__);
 
 #define IGNORE_TEST_CASE(TC) void ignore_tc_##TC()
@@ -36,6 +37,29 @@
 } \
 static test::Test_##TC tc_##TC;                                     \
 void test::Test_##TC::execute()                                 \
+
+
+#define FIXTURE_TEST_CASE(TC, Fixture) namespace test { struct Test_##TC  : public Fixture             \
+{                                                   \
+    void execute();                                 \
+                                                    \
+    void registerMe()                               \
+    {                                               \
+        detail::TestRunner::instance().registerTest(        \
+                     #TC,                           \
+                     std::bind(&Test_##TC::execute, this)   \
+                );                                  \
+    }                                               \
+                                                    \
+    Test_##TC()                                          \
+    {                                               \
+        registerMe();                               \
+    }                                               \
+};                                                  \
+} \
+static test::Test_##TC tc_##TC;                                     \
+void test::Test_##TC::execute()                                 \
+
 
 #define RUN_TESTS() test::detail::TestRunner::instance().runTests()
 
