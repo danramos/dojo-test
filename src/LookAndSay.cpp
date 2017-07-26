73f0e4805501745e5ed631fdb2fa9c8fdaf55e0d
daramos <dan.ramos@nokia.com>
2017-07-26 14:10:20 +0800

    bare problem

--- src/LookAndSay.cpp
+++ src/LookAndSay.cpp
@@ -5,21 +5,5 @@
 
 std::string LookAndSay::next(const std::string& current)
 {
-    std::stringstream ss;
-   
-    const auto length = current.length();
-    auto current_number = current[0];
-    auto consecutive_length = 1;
-    for (auto index = 1u; index < length; index++) {
-        if (current_number == current[index]) {
-            consecutive_length++;
-        } else {
-            ss << consecutive_length << current_number;
-            consecutive_length = 1;
-        }
-        current_number = current[index];
-   }
-   ss << consecutive_length << current_number;
-
-   return ss.str();
+    return "";
 }
